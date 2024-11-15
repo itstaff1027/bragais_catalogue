a5c2bc62fe9e54fe4b0d878bff57091161f8d18b
itstaff1027 <itstaffdelrosario@gmail.com>
2024-11-15 13:24:44 +0800

    Update: Npm Run Build

---
+++ public/build/assets/Page-CraQ9Rx3.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-BAxydV3v.js";import"./AuthenticatedLayout-oaZouVQP.js";import{P as r}from"./ProductsLayout-DmSQaEPp.js";import"./ApplicationLogo-CW_ZO5LO.js";function d({params:e}){return t.jsx(r,{header:t.jsx("h2",{className:"text-xl font-semibold leading-tight text-gray-800",children:"Create Product"}),children:"Create Products"})}export{d as default};
