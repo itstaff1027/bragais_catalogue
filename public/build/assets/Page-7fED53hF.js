6e0a9d4c5611fe2bbd5d0f2d6ed642cb183d6362
itstaff1027 <itstaffdelrosario@gmail.com>
2024-12-18 11:29:22 +0800

    Updating: Latest Commits npm run build

---
+++ public/build/assets/Page-7fED53hF.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-DMcxLD5v.js";import"./AuthenticatedLayout-BsNsZQgZ.js";import{T as r}from"./ToolsLayout-BsNitkV5.js";import"./ApplicationLogo-DOI9iAdj.js";import"./transition-DC9CxrFl.js";function e({params:t}){return o.jsx(r,{children:"Tools"})}export{e as default};
--- public/build/assets/Page-UIfGYHIl.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-Bcg6lVfv.js";import"./AuthenticatedLayout-BqB5sKmN.js";import{T as r}from"./ToolsLayout-CSkxdARl.js";import"./ApplicationLogo-CfB3j6k0.js";import"./transition-CLjT16kT.js";function e({params:t}){return o.jsx(r,{children:"Tools"})}export{e as default};
