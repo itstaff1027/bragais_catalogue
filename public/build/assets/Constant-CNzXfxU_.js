a4d83afef24cbfb2193359472ea9f022a3551ef3
itstaff1027 <itstaffdelrosario@gmail.com>
2025-01-10 18:58:16 +0800

    Update: for latest build

---
+++ public/build/assets/Constant-CNzXfxU_.js
@@ -0,0 +1,1 @@
+const o="https://jojobragais.catalogue.sgp1.digitaloceanspaces.com/",s="top",a="body",t="bottom",l="womens_x_scrollable",c="mens_x_scrollable";export{t as a,a as b,c as m,s as t,o as u,l as w};
